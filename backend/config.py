71d7d1f3c6da3316b061b23dbf92bc84736bf7f0
Gayeon Park <gypark@itsmart.co.kr>
2026-06-18 09:43:38 +0900

    [환경설정]
    (config.py)
    - 이미 Docker compose 에서 .env 파일로 환경변수를 설정하고 있으므로  중복되는 load_dotenv() 제거.
    - Docker 의 환경변수값을 세팅하기 위해 getenv() -> environ.get() 으로 변경
    (.env)
    - FASTAPI_HOST,PORT 는 미사용 환경변수로 제거.

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,9 @@
 import os
+
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
-from dotenv import load_dotenv
+
 
 @dataclass(frozen=True)
 class Settings:
@@ -16,17 +17,40 @@ class Settings:
     top_k: int
     upload_dir: Path
 
+
 @lru_cache
 def get_settings() -> Settings:
-    load_dotenv()
-    return Settings(
-        ollama_base_url=os.getenv("OLLAMA_BASE_URL", "http://codeMind-ollama:11434"),
-        ollama_model=os.getenv("OLLAMA_MODEL", "qwen2.5-coder:3b"),
-        qdrant_url=os.getenv("QDRANT_URL", "http://codeMind-qdrant:6333"),
-        qdrant_collection=os.getenv("QDRANT_COLLECTION", "source_chunks"),
-        embedding_model=os.getenv("EMBEDDING_MODEL", "BAAI/bge-m3"),
-        chunk_size=int(os.getenv("CHUNK_SIZE", 800)),
-        chunk_overlap=int(os.getenv("CHUNK_OVERLAP", 100)),
-        top_k=int(os.getenv("TOP_K", 5)),
-        upload_dir=Path(os.getenv("UPLOAD_DIR", "/data/uploads"))
+    return Settings( # os.environ.get() 로 Docker 환경변수의 설정값을 가져옴.
+        ollama_base_url=os.environ.get(
+            "OLLAMA_BASE_URL",
+            "http://codeMind-ollama:11434"
+        ),
+        ollama_model=os.environ.get(
+            "OLLAMA_MODEL",
+            "qwen2.5-coder:3b"
+        ),
+        qdrant_url=os.environ.get(
+            "QDRANT_URL",
+            "http://codeMind-qdrant:6333"
+        ),
+        qdrant_collection=os.environ.get(
+            "QDRANT_COLLECTION",
+            "source_chunks"
+        ),
+        embedding_model=os.environ.get(
+            "EMBEDDING_MODEL",
+            "BAAI/bge-m3"
+        ),
+        chunk_size=int(
+            os.environ.get("CHUNK_SIZE", 800)
+        ),
+        chunk_overlap=int(
+            os.environ.get("CHUNK_OVERLAP", 100)
+        ),
+        top_k=int(
+            os.environ.get("TOP_K", 5)
+        ),
+        upload_dir=Path(
+            os.environ.get("UPLOAD_DIR", "/data/uploads")
+        )
     )
